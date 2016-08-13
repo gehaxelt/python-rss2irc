d61bc5d2d5dad607332bfe131e537d139011ab57
Jerome Charaoui <jerome@riseup.net>
2016-08-13 10:55:21 -0400

    Allow program termination using SIGINT
    It's a rather crude method, but it works.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from bot import Bot
+import os
+import signal
+
+def signal_handler(signal, frame):
+    print "Caught SIGINT, terminating."
+    os._exit(0)
 
 if __name__ == "__main__":
     bot = Bot()
     bot.start()
+    signal.signal(signal.SIGINT, signal_handler)
+    while Ture:
+        signal.pause()
