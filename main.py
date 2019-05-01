53613d80e096e987bc73eb843c7ab8215ac2a84c
Wibol <19147332+Wibol@users.noreply.github.com>
2019-05-01 12:45:02 +0200

    Update main.py
    utf-8

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
 from bot import Bot
 from feedupdater import FeedUpdater
+from config import Config
 import os
 import signal
+import datetime
+import sys
+import codecs
+import locale
+import time
+
+sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
+sys.stderr = codecs.getwriter(locale.getpreferredencoding())(sys.stderr)
+Config.lastpubmsg = time.time()
 
 def signal_handler(signal, frame):
-    print "Caught SIGINT, terminating."
+    print datetime.datetime.now() , u"Received SIGINT signal, finishing bot."
+    sys.stdout.flush()
     os._exit(0)
 
 if __name__ == "__main__":
@@ -15,7 +27,8 @@ if __name__ == "__main__":
     missing_config_keys = bot.get_missing_options()
     if not len(missing_config_keys) == 0:
         for key in missing_config_keys:
-            print "Config option '{}' is missing! Please check your config!".format(key)
+            print datetime.datetime.now(), u"The '{}' option isn't set up! Check configuration.".format(key)
+            sys.stdout.flush()
         os._exit(1)
 
     bot._Bot__irc.connection.buffer_class.errors = 'replace' # prevent utf-8 error in jaraco.stream
