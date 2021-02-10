1781a3f7139794fd5d4d69d558bce827f0bee80d
Wibol <19147332+Wibol@users.noreply.github.com>
2021-02-10 12:16:25 +0100

    Translated to Python3

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 from bot import Bot
 from feedupdater import FeedUpdater
@@ -8,14 +7,9 @@ import os
 import signal
 import datetime
 import sys
-import codecs
-import locale
-
-sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
-sys.stderr = codecs.getwriter(locale.getpreferredencoding())(sys.stderr)
 
 def signal_handler(signal, frame):
-    print datetime.datetime.now() , u"Received SIGINT signal, finishing bot."
+    print(datetime.datetime.now() , "Received SIGINT signal, finishing bot.")
     sys.stdout.flush()
     os._exit(0)
 
@@ -25,7 +19,7 @@ if __name__ == "__main__":
     missing_config_keys = bot.get_missing_options()
     if not len(missing_config_keys) == 0:
         for key in missing_config_keys:
-            print datetime.datetime.now(), u"The '{}' option isn't set up! Check configuration.".format(key)
+            print(datetime.datetime.now(), "The '{}' option isn't set up! Check configuration.".format(key))
             sys.stdout.flush()
         os._exit(1)
 
